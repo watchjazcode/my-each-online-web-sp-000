78726fefd537db023d7a0b2f4401a3b0a9147ba3
Jazmin M <jazminmerckel@gmail.com>
2019-10-02 02:35:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,3 +2,17 @@ def my_each(array)
   # code here
 end
 
+def hello_t(array)
+  if block_given?
+    i = 0 
+  
+    while i < array.length 
+      yield array[i] #yield keyword is running the code below when hello_t is called
+      i = i + 1 
+    end
+  
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end

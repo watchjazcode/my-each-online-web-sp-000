c85c05e89ab05549fd35cc94311d5ee19bdb6fb1
Jazmin M <jazminmerckel@gmail.com>
2019-10-02 03:06:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -14,9 +14,17 @@
 # after you know it was given, you can use yield keyword, any parameters given to the yield keyword will become parameters of the block 
 
 def my_each(array)
+  if block_given?
+    i = 0
   
-  while 
-
+    while i < array.length 
+      yield array[i] 
+      i = i + 1 
+    end
+  
+    array
+  else
+    puts "Hey! No block was given!"
   end
 end
 
